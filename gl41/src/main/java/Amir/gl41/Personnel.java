1e0a38da702235a6fa5c45157777e336886964ea
Amir <malkhawarizmi0@gmail.com>
2020-03-04 12:37:32 +0100

    class personnel immuable

---
+++ gl41/src/main/java/Amir/gl41/Personnel.java
@@ -0,0 +1,15 @@
+package Amir.gl41;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public final class Personnel {
+	private String Nom;
+	private String Prenom;
+	private String fonctions;
+	private DateTimeFormatter germanFormatter;
+    LocalDate Date_naissance  = LocalDate.parse("24.12.2014", germanFormatter);
+    private List<Integer> numero_tel;
+
+}
